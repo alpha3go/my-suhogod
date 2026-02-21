24a55d75cfb6076c6464c186fedd042534c3a107
alpha3go <alpha3go@naver.com>
2026-02-21 15:53:03 +0900

    fix: Add webpack chunk splitting for Cloudflare 25MB limit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          chunks: 'all',
+          cacheGroups: {
+            default: false,
+            vendors: false,
+            commons: {
+              name: 'commons',
+              chunks: 'all',
+              minChunks: 2,
+            },
+            lib: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module: { context: string }) {
+                const match = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/);
+                const packageName = match ? match[1] : 'vendors';
+                return `npm.${packageName.replace('@', '')}`;
+              },
+              priority: 10,
+            },
+          },
+        },
+      };
+    }
+    return config;
+  },
+  experimental: {
+    optimizePackageWarn: false,
+  },
 };
 
 export default nextConfig;
